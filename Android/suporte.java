331a190f193c8bce9c1757e1d282ceb8bd622ef1
lloyd4565 <88751177+lloyd4565@users.noreply.github.com>
2021-11-03 18:14:04 +0200

    Update suporte.java

--- Android/suporte.java
+++ Android/suporte.java
@@ -1,9 +1,9 @@
 /*
- * Classname             (darkbot3.0)
+ * Classname             (masala3.0)
  *
  * Version information   (3.0)
  *
  * Date                  (06/12,15:35)
  *
- * author                (Dark YT)
+ * author                (nuisance)
  * Copyright notice      (bot com suporte a Android/Java)
